8b8f54a4622e7148805026f060c06f0322e2a506
sangkkim <madlogic365@gmail.com>
2022-05-24 18:22:42 +0900

    change some goal points

--- bazu/trajectory_gen.py
+++ bazu/trajectory_gen.py
@@ -12,7 +12,7 @@ class Trajectory_publisher(Node):
         timer_period = 1
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.joints = ['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6']
-        self.goal_positions = [0.5,0.5,0.5,0.5,0.3,0.1]
+        self.goal_positions = [10.0,-1.0,0.0,0.0,0.0,0.0]
 
 
     def timer_callback(self):
